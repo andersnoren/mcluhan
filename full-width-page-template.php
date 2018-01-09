99c268db82780373858f6326cd4bb23efa1d1a78
Anders <anders.noren@24hr.se>
2018-01-09 08:12:06 +0100

    Replaced locate_template() include with get_template_part()

--- full-width-page-template.php
+++ full-width-page-template.php
@@ -4,4 +4,4 @@
 
 /* We only need the page template specific body_class for styling – other than that, the page template is exactly the same as all other singulars. Because of this, we can just include the singular template here. */
 
-locate_template( 'singular.php', true ); ?>
+get_template_part( 'singular' ); ?>
